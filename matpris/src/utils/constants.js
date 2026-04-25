9445799333111cb615aeec4c6129a46b558b4d9d
erlensam <erlensam@stud.ntnu.no>
2026-04-25 16:06:10 +0200

    changed coop prix colors

--- matpris/src/utils/constants.js
+++ matpris/src/utils/constants.js
@@ -11,9 +11,9 @@
 export const STORES = {
   rema:       { name: "Rema 1000",  color: "#0060A9", bg: "#E8F1F8" },
   kiwi:       { name: "Kiwi",       color: "#6B9B1E", bg: "#EFF5E5" },
-  coop_prix:  { name: "Coop Prix",  color: "#E2001A", bg: "#FDEBEE" },
+  coop_prix:  { name: "Coop Prix",  color: "#0060A9", bg: "#fee987cc" },
   coop_extra: { name: "Coop Extra", color: "#E2001A", bg: "#FDEBEE" },
-  coop_mega:  { name: "Coop Mega",  color: "#E2001A", bg: "#FDEBEE" },
+  coop_mega:  { name: "Coop Mega",  color: "#007A3D", bg: "#E5F2EB" },
   meny:       { name: "Meny",       color: "#C8102E", bg: "#FAE9EC" },
   bunnpris:   { name: "Bunnpris",   color: "#F7941E", bg: "#FEF3E3" },
   joker:      { name: "Joker",      color: "#1B3668", bg: "#E8ECF3" },
